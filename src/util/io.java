2281c48375ece71900d05d61fdf70d86483adbce
Aaron Zhong <aaron_zhong2b@hotmail.com>
2016-08-04 12:55:52 +1200

    inital commit

---
+++ src/util/io.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Created by jay on 1/08/16.
+ * This is just a skeleton class that I've created to test some input and output of the DOT format file. All utility classes
+ * should be included in the "util" package.
+ */
+public class io {
+}
