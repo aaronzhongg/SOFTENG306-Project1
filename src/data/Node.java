2f15a67d6fce17dce5d4b6a5de320343709aa924
Humbis <alex1041227101@gmail.com>
2016-08-05 13:36:54 +1200

    added a processor field to nodes

--- src/data/Node.java
+++ src/data/Node.java
@@ -3,10 +3,12 @@ package data;
 import java.util.ArrayList;
 
 public class Node {
+	//Using public instead of getter and setters might be faster
 	public String nodeName;
 	public int nodeWeight;
 	public ArrayList<Edge> edges;
-	public ArrayList<String> prevNodes; //This is a list of nodes that have to complete before this can start
+	public ArrayList<String> prevNodes; 	//This is a list of nodes that have to complete before this can start
+	public int processorNo; 				//This will be the processor number assigned to the node in the algorithm
 	
 	public Node(String name, int weight){
 		nodeName = name;
