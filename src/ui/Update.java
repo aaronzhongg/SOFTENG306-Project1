eac6d0c6c9c812951cc45d6842a2c3fad98bf722
Jingw98 <sherry-wang95@hotmail.com>
2016-08-10 19:40:14 +1200

    refine colors

--- src/ui/Update.java
+++ src/ui/Update.java
@@ -10,24 +10,42 @@ public class Update {
 		this.processorNum=processorNum;
 	}
 	public void updateColor(int id,int processor,Graph g){
-		for(int i=0;i<processorNum;i++){
-			
-			
+					
+			switch (processor){
+			case 1: 
+				g.getNode(id).addAttribute("ui.style", "fill-color: red;");
+				break;
+			case 2:
+				g.getNode(id).addAttribute("ui.style", "fill-color: blue;");
+				break;
+			case 3:
+				g.getNode(id).addAttribute("ui.style", "fill-color: yellow;");
+				break;
+			case 4:
+				g.getNode(id).addAttribute("ui.style", "fill-color: green;");
+				break;
+			case 5:
+				g.getNode(id).addAttribute("ui.style", "fill-color: magenta;");
+				break;
+			case 6:
+				g.getNode(id).addAttribute("ui.style", "fill-color: cyan;");
+				break;
+			case 7:
+				g.getNode(id).addAttribute("ui.style", "fill-color: brown;");
+				break;
+			}
 			//System.out.println(processor+"??");
-			if(processor==i){
-			
-			int colorR=255/(i+1);
 			
 			
 			
 			//System.out.println(color);
-			g.getNode(id).addAttribute("ui.style", "fill-color: rgb("+colorR+",100,255);");
+			
 		}
-		}
+		
 		
 		
 	}
 	
 	
 	
-}
+
